aad420ee6f202db6c55a5bd2abda4c0e4019621e
Foukfik <gamumum_123@hotmail.com>
2019-07-01 12:47:25 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String args[])
+	{
+//		int a = 10;
+//		double price = 10.5;
+//		char c = 'a';
+//		String name = "foukfik";
+//		System.out.println("Price : " + price);
+//		//System.out.println(name);
+//		
+//		int[] num= {1,2,3,4};
+//		
+//		for(int i = 0 ; i < num.length ; i++) {
+//			System.out.println(num[i]);
+//		}
+//		
+//		int[] num2 = new int[100];
+//		String[] fruits = {"banana","apple"};
+//		
+//		for(String f : fruits)
+//			System.out.println(f);
+		String tel = "053-211-222";
+		String[] ts = tel.split("-");
+		for(String t : ts)
+		System.out.println(t);
+	}
+}
